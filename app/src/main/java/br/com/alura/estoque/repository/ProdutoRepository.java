0f13e513d4e87a52e40156c447c0425e5f661606
Rafael Moura <rafael.g.moura@hotmail.com>
2023-03-07 13:32:42 -0300

    webapi 4-4 Salvando produto na API

--- app/src/main/java/br/com/alura/estoque/repository/ProdutoRepository.java
+++ app/src/main/java/br/com/alura/estoque/repository/ProdutoRepository.java
@@ -9,13 +9,16 @@ import br.com.alura.estoque.model.Produto;
 import br.com.alura.estoque.retrofit.EstoqueRetrofit;
 import br.com.alura.estoque.retrofit.service.ProdutoService;
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
 
 public class ProdutoRepository {
     private final ProdutoDAO dao;
+    private final ProdutoService service;
 
     public ProdutoRepository(ProdutoDAO dao) {
         this.dao = dao;
+        service = new EstoqueRetrofit().getProdutoService();
     }
 
 
@@ -39,7 +42,7 @@ public class ProdutoRepository {
     }
 
     private void buscaProdutosNaAPI(DadosCarregadosListener<List<Produto>> listener) {
-        ProdutoService service = new EstoqueRetrofit().getProdutoService();
+
         Call<List<Produto>> call = service.buscaTodos();
 
         new BaseAsyncTask<>(() -> {
@@ -59,14 +62,29 @@ public class ProdutoRepository {
     // fim metodos de busca de lista de produtos
 
     public void salva(Produto produto, DadosCarregadosListener<Produto> listener) {
-        new BaseAsyncTask<>(() -> {
-            long id = dao.salva(produto);
-            return dao.buscaProduto(id);
-        }, produtoSalvo ->
-                //notificacao ao adapter
-                listener.quandoCarregados(produtoSalvo)
+        Call<Produto> call = service.salva(produto);
+        call.enqueue(new Callback<Produto>() {
+            @Override
+            public void onResponse(Call<Produto> call, Response<Produto> response) {
+                Produto produtoQueFoiSalvo = response.body();
+                new BaseAsyncTask<>(() -> {
+                    long id = dao.salva(produtoQueFoiSalvo);
+                    return dao.buscaProduto(id);
+                }, produtoSalvo ->
+                        //notificacao ao adapter
+                        listener.quandoCarregados(produtoSalvo)
                 )
-                .execute();
+                        .execute();
+
+            }
+
+            @Override
+            public void onFailure(Call<Produto> call, Throwable t) {
+
+            }
+        });
+
+
     }
 
     // metodos de salvamento de produtos
